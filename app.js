64b02e536bef6533c7d1a1c35e4db476996fdd0c
Maryyann Landlord <mlandlor@gmail.com>
2015-04-17 18:58:45 -0400

    Fixed Facial Performance Update

--- app.js
+++ app.js
@@ -51,6 +51,12 @@ $(document).ready(function() {
 
 
 
+
 });
 
 
+	function onVideoEnd(data) {
+	var id = data[0];
+	html5GalleryObjects.objects[0].isPaused = true;
+	html5GalleryObjects.objects[0].slideRun(id, false, true);
+}
