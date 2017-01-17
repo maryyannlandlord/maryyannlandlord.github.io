a76785fa8ee9481a0ac0ea13afb49bee3ed93e9d
Maryyann Landlord <mlandlor@gmail.com>
2017-01-17 00:19:14 -0800

    project updates and about page

--- app.js
+++ app.js
@@ -10,51 +10,6 @@ $(document).ready(function() {
 		});
 
 
-	$("#gif_1").hover(
-		function()
-	{
-		$(this).attr("src", "Portfolio/Mojo/GIFS/block1.gif");
-	},
-		function()
-	{
-		$(this).attr("src", "Portfolio/Mojo/gif_cover/block1.png");
-	}
-	);
-
-	$("#gif_2").hover(
-		function()
-	{
-		$(this).attr("src", "Portfolio/Mojo/GIFS/block2.gif");
-	},
-		function()
-	{
-		$(this).attr("src", "Portfolio/Mojo/gif_cover/block2.png");
-	}
-	);
-
-	$("#gif_3").hover(
-		function()
-	{
-		$(this).attr("src", "Portfolio/Mojo/GIFS/block3.gif");
-	},
-		function()
-	{
-		$(this).attr("src", "Portfolio/Mojo/gif_cover/block3.png");
-	}
-	);
-
-	$("#gif_4").hover(
-		function()
-	{
-		$(this).attr("src", "Portfolio/Mojo/GIFS/wake_up.gif");
-	},
-		function()
-	{
-		$(this).attr("src", "Portfolio/Mojo/gif_cover/wake_up.png");
-	}
-	);
-
-
 
 
 });
