47547950a6166d54fb56e5f4c047f2098d619e39
Maryyann Landlord <mlandlor@gmail.com>
2022-06-03 21:50:19 -0700

    Big Website Layout Update
    updated home, proj, sketches pages

---
+++ Galleries.js
@@ -0,0 +1,279 @@
+
+const wetwareTexture = [
+            {
+                src: 'Portfolio/Wetware/tex13.jpg',
+                thumb: 'Portfolio/Wetware/tex13.jpg',
+                 
+            },
+            {
+                src: 'Portfolio/Wetware/tex11.png',
+                thumb: 'Portfolio/Wetware/tex11.png',
+                
+            },
+            {
+                src: 'Portfolio/Wetware/still20.jpg',
+                thumb: 'Portfolio/Wetware/still20.jpg',
+                
+            },
+             {
+                src: 'Portfolio/Wetware/tex8.jpg',
+                thumb: 'Portfolio/Wetware/tex8.jpg',
+                
+            },
+            {
+                src: 'Portfolio/Wetware/tex10.jpg',
+                thumb: 'Portfolio/Wetware/tex10.jpg',
+                
+            },
+            {
+                src: 'Portfolio/Wetware/tex14.jpg',
+                thumb: 'Portfolio/Wetware/tex14.jpg',
+                
+            },
+        ];
+
+
+const wetwareStills = [
+            {
+                src: 'Portfolio/Wetware/still19.png',
+                thumb: 'Portfolio/Wetware/still19.png',
+                 
+            },
+            {
+                src: 'Portfolio/Wetware/still10.jpg',
+                thumb: 'Portfolio/Wetware/still10.jpg',
+                
+            },
+            {
+                src: 'Portfolio/Wetware/still14.jpg',
+                thumb: 'Portfolio/Wetware/still14.jpg',
+                
+            },
+             {
+                src: 'Portfolio/Wetware/still15.jpg',
+                thumb: 'Portfolio/Wetware/still15.jpg',
+                
+            },
+            {
+                src: 'Portfolio/Wetware/still18.png',
+                thumb: 'Portfolio/Wetware/still18.png',
+                
+            },
+            {
+                src: 'Portfolio/Wetware/still8.jpg',
+                thumb: 'Portfolio/Wetware/still8.jpg',
+                
+            },
+             {
+                src: 'Portfolio/Wetware/still7.jpg',
+                thumb: 'Portfolio/Wetware/still7.jpg',
+                
+            },
+             {
+                src: 'Portfolio/Wetware/still9.jpg',
+                thumb: 'Portfolio/Wetware/still9.jpg',
+                
+            }
+        ];
+
+const wetwareConcepts = [
+            {
+                src: 'Portfolio/Wetware/art3.png',
+                thumb: 'Portfolio/Wetware/art3.png',
+                 
+            },
+            {
+                src: 'Portfolio/Wetware/art4.png',
+                thumb: 'Portfolio/Wetware/art4.png',
+                
+            },
+            {
+                src: 'Portfolio/Wetware/art5.jpg',
+                thumb: 'Portfolio/Wetware/art5.jpg',
+                
+            },
+             {
+                src: 'Portfolio/Wetware/art6.jpg',
+                thumb: 'Portfolio/Wetware/art6.jpg',
+                
+            },
+          
+             {
+                src: 'Portfolio/Wetware/art9.png',
+                thumb: 'Portfolio/Wetware/art9.png',
+                
+            },
+             {
+                src: 'Portfolio/Wetware/art10.jpg',
+                thumb: 'Portfolio/Wetware/art10.jpg',
+                
+            },
+            {
+                src: 'Portfolio/Wetware/art11.jpg',
+                thumb: 'Portfolio/Wetware/art11.jpg',
+                
+            }
+        ];
+
+const IPADUI = [
+    {src: 'Portfolio/Wetware/ipad-UI.png',
+     }
+    ];
+
+const yokaiStills = [
+     {
+        src: 'Portfolio/Yokai/still5.jpg',
+        thumb: 'Portfolio/Yokai/still5.jpg',
+     },
+     
+     {
+        src: 'Portfolio/Yokai/still1.jpg',
+        thumb: 'Portfolio/Yokai/still1.jpg',
+     },
+
+     {
+        src: 'Portfolio/Yokai/still2.jpg',
+        thumb: 'Portfolio/Yokai/still2.jpg',
+     },
+    {
+        src: 'Portfolio/Yokai/still3.jpg',
+        thumb: 'Portfolio/Yokai/still3.jpg',
+     },
+     {
+        src: 'Portfolio/Yokai/still4.jpg',
+        thumb: 'Portfolio/Yokai/still4.jpg',
+     },
+      {
+        src: 'Portfolio/Yokai/still6.JPG',
+        thumb: 'Portfolio/Yokai/still6.JPG',
+     }
+];
+
+const maadStills = [
+    
+   
+    {
+        src : "Portfolio/Maad/still4.jpg",
+        thumb: "Portfolio/Maad/still4.jpg"
+    },
+     {
+        src : "Portfolio/Maad/still3.png",
+        thumb: "Portfolio/Maad/still3.png"
+    },
+    {
+        src : "Portfolio/Maad/still7.jpg",
+        thumb: "Portfolio/Maad/still7.jpg"
+    },
+     {
+        src : "Portfolio/Maad/still2.jpg",
+        thumb: "Portfolio/Maad/still2.jpg"
+    },
+    {
+        src : "Portfolio/Maad/still5.png",
+        thumb: "Portfolio/Maad/still5.png"
+    },
+
+   
+     {
+        src : "Portfolio/Maad/still6.jpg",
+        thumb: "Portfolio/Maad/still6.jpg"
+    },
+
+];
+
+const maadTech = [
+    {
+        src : "Portfolio/Maad/install2.jpg",
+        thumb: "Portfolio/Maad/install2.jpg"
+    },
+    {
+        src : "Portfolio/Maad/install1.jpg",
+        thumb: "Portfolio/Maad/install1.jpg"
+    },
+ 
+    {
+        src : "Portfolio/Maad/install4.jpg",
+        thumb: "Portfolio/Maad/install4.jpg"
+    },
+    {
+        src : "Portfolio/Maad/install3.jpg",
+        thumb: "Portfolio/Maad/install3.jpg"
+    },
+    {
+        src : "Portfolio/Maad/install5.jpg",
+        thumb: "Portfolio/Maad/install5.jpg"
+    },
+
+];
+
+
+
+function createGallery(array, elementID){
+const lgContainer = document.getElementById(elementID);
+const inlineGallery = lightGallery(lgContainer, {
+        container: lgContainer,
+        dynamic: true,
+        hash: false,
+        closable: false,
+        showMaximizeIcon: true,
+        appendSubHtmlTo: '.lg-item',
+        slideDelay: 400,
+        plugins: [lgZoom, lgThumbnail],
+        dynamicEl: array
+    });
+
+return inlineGallery; 
+}
+
+
+function createAllInlineGalleries(){
+
+    const galleries = [
+    //createGallery(wetwareTexture,"wetware-texture"),
+    //createGallery(wetwareStills,"wetware-stills"),
+    createGallery(wetwareConcepts,"wetware-concepts"),
+    createGallery(IPADUI, "IPADUI"),
+    //createGallery(yokaiStills,"yokai-stills"),
+    //createGallery(maadStills,"maad-stills"),
+    //createGallery(maadTech, "maad-tech"),
+    ]
+ 
+
+    for (let step = 0; step < galleries.length; step++){
+        if (galleries[step].el) {
+            galleries[step].openGallery();
+        }
+    }
+}
+
+createAllInlineGalleries();
+
+
+
+function createAllLightGalleries(){
+
+    const galleries = [
+    "CafeGallery", 
+    "maad-tech", 
+    "maad-stills",
+    "yokai-stills",
+    "wetware-tex",
+    "wetware-stills",
+    "momento-paintedCorner",
+    "sketchGallery"
+    ];
+
+    for (let step = 0; step < galleries.length; step++){
+
+    let gallery = document.getElementById(galleries[step]); 
+        if (gallery){
+            lightGallery(gallery,{
+            thumbnail:true,
+            plugins:  [lgThumbnail,lgVideo,lgVimeoThumbnail],            
+            });
+        };
+    }
+}
+
+createAllLightGalleries();
+
--- app.js
+++ app.js
@@ -12,12 +12,14 @@ $(function() {
        $('#reelWrapper').css('height',browser_height);
    }
 
-   $('.other_proj a').hover(function(){
-
-
+   $('.other_proj').mouseover(function(){
    	$('.text_wrapper', this).css("opacity","1");
-   },function(){
+    $('.other_proj_photo', this).css("opacity",".3");
+   });
+
+    $('.other_proj').mouseout(function(){
    	$('.text_wrapper', this).css("opacity","0");
+    $('.other_proj_photo', this).css("opacity","1");
    });
 
 // Index page projects
@@ -26,7 +28,7 @@ $(function() {
       $(this).wrap('<div class="DarkTint"></div>');
     });
 
-// Index page Wetware
+// Index page wide
 
     $('.proj_photo_full').each(function() {
       $(this).wrap('<div class="DarkTint"></div>');
@@ -34,19 +36,19 @@ $(function() {
 
 //Index page names
 
-   $('.proj_column').mouseover(function(){
+   $('.projCover').mouseover(function(){
     $('.proj_title', this).css("opacity","1");
     $('.proj_photo', this).css("opacity",".3");
     $('.proj_photo_full', this).css("opacity",".3");
    });
 
-   $('.proj_column').mouseout(function(){
+   $('.projCover').mouseout(function(){
     $('.proj_title', this).css("opacity","0");
     $('.proj_photo', this).css("opacity","1");
     $('.proj_photo_full', this).css("opacity","1");
    });
 
-// Reel
+// Reel 
 
    $('.VideoCover').wrap('<div id = "darkVideo"></div>');
 
@@ -89,6 +91,7 @@ $(function() {
     $('.videoTitle.2').css("opacity","0");
     $('.VideoCover.2', this).css("opacity", "1");
    });
+ 
 
     var videoOpen = false;
     var video2Open = false;
@@ -100,30 +103,32 @@ $(function() {
         };
       });
 
-   //  <iframe width="560" height="315" src="https://www.youtube.com/embed/p4V_Izk1xbw?controls=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+   
 
-
-    $('.videoWrapper').click(function(){
+    $('.videoWrapper.1').click(function(){
       if (videoOpen == false) {
         $("#yokai").html("<div class='embed-container'><iframe src='https://player.vimeo.com/video/252673806?autoplay=1&autopause=0' width='1500' height='844' frameborder='0' webkitallowfullscreen mozallowfullscreen allowfullscreen allow=autoplay></iframe></div>");
         $("#maad").html("<div class='embed-container'><iframe src='https://player.vimeo.com/video/326502284?autoplay=1&autopause=0' width='1500' height='844' frameborder='0' webkitallowfullscreen mozallowfullscreen allowfullscreen allow=autoplay></iframe></div>");
-        $("#momento1").html("<div class='embed-container'><iframe src='https://player.vimeo.com/video/326499057?autoplay=1&autopause=0' width='1500' height='844' frameborder='0' webkitallowfullscreen mozallowfullscreen allowfullscreen allow=autoplay></iframe></div>");
+        //$("#momento1").html("<div class='embed-container'><iframe src='https://player.vimeo.com/video/326499057?autoplay=1&autopause=0' width='1500' height='844' frameborder='0' webkitallowfullscreen mozallowfullscreen allowfullscreen allow=autoplay></iframe></div>");
         $("#steelcase").html("<div class='embed-container'><iframe src='https://player.vimeo.com/video/258761583?autoplay=1&autopause=0' width='1500' height='844' frameborder='0' webkitallowfullscreen mozallowfullscreen allowfullscreen allow=autoplay></iframe></div>");
         $("#wetware").html("<div class='embed-container'><iframe src='https://player.vimeo.com/video/340127291?autoplay=1&autopause=0' width='1500' height='844' frameborder='0' webkitallowfullscreen mozallowfullscreen allowfullscreen allow=autoplay></iframe></div>");
         $("#musicEffects").html("<div class='embed-container'><iframe src='https://www.youtube.com/embed/MDh6Z1Qm_CM?autoplay=1&autopause=0' frameborder='0'; allow=\"fullscreen;autoplay\"></iframe></div>");
+        $("#momento2").html("<div class='embed-container'><iframe src='https://www.youtube.com/embed/p4V_Izk1xbw?autoplay=1&autopause=0' frameborder='0'; allow=\"fullscreen;autoplay\"></iframe></div>");
        
         videoOpen = true;
         };
       });
 
     
-      $('.videoWrapper_2').click(function(){
+      $('.videoWrapper.2').click(function(){
       if (video2Open == false) {
-         //$("#momento2").html("<video autoplay controls style = 'width: 1500px; height: 844px' > <source src = 'Portfolio/Momento/Video2.mp4' type = 'video/mp4'></video>");
+           $("#momento3").html("<div class='embed-container'><iframe src='https://player.vimeo.com/video/326499370?autoplay=1&autopause=0' width='1500' height='844' frameborder='0' webkitallowfullscreen mozallowfullscreen allowfullscreen allow=autoplay></iframe></div>");
          
         video2Open = true;
         };
       });
+
+ 
 
 });
 
@@ -142,4 +147,28 @@ function onVideoEnd(data) {
 var id = data[0];
 html5GalleryObjects.objects[0].isPaused = true;
 html5GalleryObjects.objects[0].slideRun(id, false, true);
+};
+
+const sections = document.querySelectorAll(".section");
+const navLi = document.querySelectorAll("#flex-tableContents a");
+
+
+
+window.onscroll = () => {
+  var current = "";
+
+
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    if (pageYOffset >= sectionTop - 170) {
+      current = section.getAttribute("id"); }
+  });
+
+  navLi.forEach((a) => {
+    a.classList.remove("active");
+    if (a.getAttribute("href") == "#" + current) {
+      a.classList.add("active");
+    }
+  });
 };
